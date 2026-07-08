67793b13260dfae3a924be861c93a912372ca024
harshavardhanbonu <bonuharshavardhan@gmail.com>
2026-07-08 14:44:08 +0530

    Delete backend/prisma directory

--- backend/prisma/migrations/20251012141420_added_post_cnt/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `post_cnt` INTEGER NOT NULL DEFAULT 0;
--- backend/prisma/migrations/20251027135522_updated_post/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [MEDIA] on the enum `Post_post_type` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `post` MODIFY `post_type` ENUM('TEXT', 'IMAGE', 'VIDEO') NOT NULL;
