9c571d3754b524b2949b661ec40b46aefa8711c0
harshavardhanbonu <bonuharshavardhan@gmail.com>
2026-07-09 14:27:01 +0530

    Update jwt.js

--- backend/src/config/jwt.js
+++ backend/src/config/jwt.js
@@ -1,5 +1,8 @@
-// src/config/jwt.js
 import dotenv from "dotenv";
 dotenv.config();
 
-export const JWT_SECRET = process.env.JWT_SECRET ?? "login_signup";
+if (process.env.NODE_ENV === "production" && !process.env.JWT_SECRET) {
+  throw new Error("FATAL ERROR: JWT_SECRET is not defined.");
+}
+
+export const JWT_SECRET = process.env.JWT_SECRET || "login_signup";
